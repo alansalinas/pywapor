97dae9acdf8c4d26f4090801926969604ee2a636
bert.coerver <bertcoerver@users.noreply.github.com>
2021-06-22 10:40:42 +0200

    Added run examples.

---
+++ examples/composite_MODIS.py
@@ -0,0 +1,72 @@
+"""
+The MODIS composite FRAME run can be used if an 8-daily 
+composite product is required based on MODIS data. 
+Temporal and spatial interpolation is applied, and 
+clouds in the input data are removed and replaced. 
+The interpolation is done as described by FRAME. 
+The relative soil moisture is used for the interpolation. 
+In total 4 steps are required. The first two steps are the 
+same as the Normal MODIS run. After the Normal MODIS run the 
+calculated relative soil moisture is used (output of WAPOR) to 
+apply the interpolation in step 3. After this interpolation the 
+ETLook model is runned another time but now with 
+the interpolated data.
+"""
+import os 
+import pandas as pd
+import datetime
+import pyWAPOR
+import watertools
+
+# inputs
+Startdate = "2019-07-05"
+Enddate = "2019-07-11"
+latlim = [28.7698, 29.7576]
+lonlim = [30.2393, 31.2350]
+output_folder = r"/Users/hmcoerver/Downloads/composite_MODIS2"
+
+dates_8d = watertools.Collect.MOD11.DataAccess.Make_TimeStamps(Startdate, Enddate)
+dates = pd.date_range(dates_8d[0], (dates_8d[-1] + datetime.timedelta(days=7)).strftime("%Y-%m-%d"), freq = "D")
+Enddate = dates[-1].strftime("%Y-%m-%d")
+Startdate = dates[0].strftime("%Y-%m-%d")
+
+# Collect data for the whole daily MODIS period
+pyWAPOR.Pre_ETLook.main(
+                        output_folder, 
+                        Startdate, 
+                        Enddate, 
+                        latlim, 
+                        lonlim,
+                        LandCover = "GlobCover", 
+                        Short_Downwards_Radiation = "MERRA",
+                        composite = False, 
+                        RAW_folder= os.path.join(output_folder, "RAW"),
+                       ) 
+                       
+
+# Run pyWAPOR model over daily MODIS data
+print(dates)
+for date in dates:
+    pyWAPOR.ETLook.ETLook_code.main(os.path.join(output_folder, "ETLook_input"), 
+                                    os.path.join(output_folder, "ETLook_output"),
+                                    date)
+
+pyWAPOR.Calculate_composite_MODIS.main(
+                                       output_folder, 
+                                       Startdate,
+                                       Enddate,
+                                       latlim,
+                                       lonlim,
+                                       os.path.join(output_folder, "ETLook_input"), 
+                                       os.path.join(output_folder, "ETLook_output"), 
+                                       LandCover = "GlobCover",
+                                       Short_Downwards_Radiation = "MERRA", 
+                                       RAW_folder= os.path.join(output_folder, "RAW"),
+                                      )
+
+dates_8d = watertools.Collect.MOD11.DataAccess.Make_TimeStamps(Startdate, Enddate)
+
+for date in dates_8d:
+    pyWAPOR.ETLook.ETLook_code.main(os.path.join(output_folder, "ETLook_input_composite_MODIS"), 
+                                    os.path.join(output_folder, "ETLook_output_composite_MODIS"),
+                                    date)
---
+++ examples/normal_MODIS.py
@@ -0,0 +1,42 @@
+"""
+The Normal MODIS run can be used if daily data 
+is required based on MODIS data. No temporal 
+or spatial interpolation is used, and clouds 
+in the input data are removed and not replaced. 
+The run exists in two parts. Part one is to download 
+and process all the required data. This will create 
+the input folder. The second part will run the ETLook 
+model by using this input folder. The data is stored in 
+the Output folder.
+"""
+import os
+import pandas as pd
+import pyWAPOR
+
+# inputs
+Startdate = "2019-07-01"
+Enddate = "2019-07-31"
+latlim = [28.5, 31.9]
+lonlim = [29.2, 32.5]
+output_folder = r"/Users/hmcoerver/Downloads/normal_MODIS"
+
+dates = pd.date_range(Startdate, Enddate, freq = "D")
+
+# Collect data for the whole daily MODIS period
+pyWAPOR.Pre_ETLook.main(
+                        output_folder, 
+                        Startdate, 
+                        Enddate, 
+                        latlim, 
+                        lonlim,
+                        LandCover = "GlobCover", 
+                        Short_Downwards_Radiation = "MERRA",
+                        composite = False, 
+                        RAW_folder= os.path.join(output_folder, "RAW"),
+                       )
+
+# Run pyWAPOR model over daily MODIS data
+for date in dates:
+    pyWAPOR.ETLook.ETLook_code.main(os.path.join(output_folder, "ETLook_input"), 
+                                    os.path.join(output_folder, "ETLook_output"),
+                                    date)
