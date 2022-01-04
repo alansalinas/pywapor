3e54c92ae19efda5f1d5c11688517fd084038aaf
bert.coerver <bertcoerver@users.noreply.github.com>
2022-01-04 16:03:27 +0100

    new datasource

---
+++ pywapor/collect/STATICS/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+"""
+from pywapor.collect.STATICS.data_access import collect
+
+__all__ = ['collect']
+
+__version__ = '0.1'
--- pywapor/collect/downloader.py
+++ pywapor/collect/downloader.py
@@ -4,31 +4,31 @@ def collect_sources(param, sources, dl_args, extra_source_locations = None):
     files = list()
     for source in sources:
 
-        if source == "PROBAV" and param == "NDVI":
+        if source == "PROBAV" and param == "ndvi":
             files.append(c.PROBAV.NDVI(**dl_args))
-        elif source == "MOD13" and param == "NDVI":
+        elif source == "MOD13" and param == "ndvi":
             files.append(c.MOD13.NDVI(**dl_args))
-        elif source == "MYD13" and param == "NDVI":
+        elif source == "MYD13" and param == "ndvi":
             files.append(c.MYD13.NDVI(**dl_args))
 
-        elif source == "PROBAV" and param == "ALBEDO":
+        elif source == "PROBAV" and param == "r0":
             files.append(c.PROBAV.ALBEDO(**dl_args))
-        elif source == "MCD43" and param == "ALBEDO":
+        elif source == "MCD43" and param == "r0":
             files.append(c.MCD43.ALBEDO(**dl_args))
 
-        elif source == "MOD11" and param == "LST":
+        elif source == "MOD11" and param == "lst":
             files.append(c.MOD11.LST(**dl_args))
-        elif source == "MYD11" and param == "LST":
+        elif source == "MYD11" and param == "lst":
             files.append(c.MYD11.LST(**dl_args))
 
-        elif source == "CHIRPS" and param == "PRECIPITATION":
+        elif source == "CHIRPS" and param == "p_24":
             files.append(c.CHIRPS.PRECIPITATION(**dl_args))
-        elif source == "SRTM" and param == "DEM":
+        elif source == "SRTM" and param == "z":
             files.append([c.SRTM.DEM(**dl_args)])
 
-        elif source == "GLOBCOVER" and param == "LULC":
+        elif source == "GLOBCOVER" and param == "lulc":
             files.append([c.GLOBCOVER.LULC(**dl_args)])
-        elif source == "WAPOR" and param == "LULC":
+        elif source == "WAPOR" and param == "lulc":
             files.append(c.WAPOR.LULC(**dl_args))
 
         elif source == "GEOS5" and param == "t_air_24":
@@ -43,7 +43,7 @@ def collect_sources(param, sources, dl_args, extra_source_locations = None):
             files.append(c.GEOS5.daily(**dl_args, Vars = ['v2m']))
         elif source == "GEOS5" and param == "qv_24":
             files.append(c.GEOS5.daily(**dl_args, Vars = ['qv2m']))
-        elif source == "GEOS5" and param == "p_air_24_0":
+        elif source == "GEOS5" and param == "p_air_0_24":
             files.append(c.GEOS5.daily(**dl_args, Vars = ['slp']))
 
         elif source == "GEOS5" and param == "t_air_i":
@@ -73,7 +73,7 @@ def collect_sources(param, sources, dl_args, extra_source_locations = None):
             files.append(c.MERRA2.daily_MERRA2(**dl_args, Vars = ['v2m']))
         elif source == "MERRA2" and param == "qv_24":
             files.append(c.MERRA2.daily_MERRA2(**dl_args, Vars = ['q2m']))
-        elif source == "MERRA2" and param == "p_air_24_0": # TODO should be "p_air_0_24"
+        elif source == "MERRA2" and param == "p_air_0_24":
             files.append(c.MERRA2.daily_MERRA2(**dl_args, Vars = ['slp']))
 
         elif source == "MERRA2" and param == "t_air_i":
@@ -91,13 +91,43 @@ def collect_sources(param, sources, dl_args, extra_source_locations = None):
         elif source == "MERRA2" and param == "p_air_0_i":
             files.append(c.MERRA2.hourly_MERRA2(**dl_args, Vars = ["slp"]))
 
-        elif source == "MERRA2" and param == "SOLAR_RADIATION":
+        elif source == "MERRA2" and param == "ra_24":
             files.append(c.MERRA2.daily_MERRA2(**dl_args, Vars = ['swgnet']))
+
+        elif source == "STATICS" and param == "land_mask":
+            files.append(c.STATICS.collect(**dl_args, vars = ['land_mask']))
+        elif source == "STATICS" and param == "lw_offset":
+            files.append(c.STATICS.collect(**dl_args, vars = ['lw_offset']))
+        elif source == "STATICS" and param == "lw_slope":
+            files.append(c.STATICS.collect(**dl_args, vars = ['lw_slope']))
+        elif source == "STATICS" and param == "r0_bare":
+            files.append(c.STATICS.collect(**dl_args, vars = ['r0_bare']))
+        elif source == "STATICS" and param == "r0_full":
+            files.append(c.STATICS.collect(**dl_args, vars = ['r0_full']))
+        elif source == "STATICS" and param == "rn_offset":
+            files.append(c.STATICS.collect(**dl_args, vars = ['rn_offset']))
+        elif source == "STATICS" and param == "rn_slope":
+            files.append(c.STATICS.collect(**dl_args, vars = ['rn_slope']))
+        elif source == "STATICS" and param == "rs_min":
+            files.append(c.STATICS.collect(**dl_args, vars = ['rs_min']))
+        elif source == "STATICS" and param == "t_amp_year":
+            files.append(c.STATICS.collect(**dl_args, vars = ['t_amp_year']))
+        elif source == "STATICS" and param == "t_opt":
+            files.append(c.STATICS.collect(**dl_args, vars = ['t_opt']))
+        elif source == "STATICS" and param == "vpd_slope":
+            files.append(c.STATICS.collect(**dl_args, vars = ['vpd_slope']))
+        elif source == "STATICS" and param == "z_obst_max":
+            files.append(c.STATICS.collect(**dl_args, vars = ['z_obst_max']))
+        elif source == "STATICS" and param == "z_oro":
+            files.append(c.STATICS.collect(**dl_args, vars = ['z_oro']))
 
         else:
             sideload_files = c.sideloader.search_product_files(source, extra_source_locations[source])
             if len(sideload_files) > 0:
                 files.append(sideload_files)
+
+        # Remove sources for which no files were downloaded.
+        files = [x for x in files if len(x) > 0]
 
     return files
 
@@ -110,33 +140,36 @@ if __name__ == "__main__":
     latlim = [28.9, 29.7]
     lonlim = [30.2, 31.2]
     startdate = "2021-07-01"
-    enddate = "2021-07-11"
+    enddate = "2021-07-03"
 
     dl_args = {
-            "Dir": os.path.join(project_folder, "RAW"), 
-            "latlim": latlim, 
-            "lonlim": lonlim, 
-            "Startdate": startdate, 
+            "Dir": os.path.join(project_folder, "RAW"),
+            "latlim": latlim,
+            "lonlim": lonlim,
+            "Startdate": startdate,
             "Enddate": enddate,
             }
 
-    raw_lst_files = collect_sources("LST", ['MOD11', 'MYD11'], dl_args, None)
-    ds_lst = pywapor.pre_se_root.combine_lst(raw_lst_files)
+    raw_r0_files = collect_sources("r0", ['MCD43'], dl_args, None)
+    raw_lst_files = collect_sources("lst", ['MOD11', 'MYD11'], dl_args, None)
+    raw_ndvi_files = collect_sources("ndvi", ['MYD13', 'MOD13'], dl_args, None)
 
-    sds, eds, prds = pywapor.pre_se_root.calc_periods(ds_lst.time.values, "3H")
+    # ds_lst = pywapor.pre_se_root.combine_lst(raw_lst_files)
 
-    dl_args["Startdate"] = sds
-    dl_args["Enddate"] = eds
-    dl_args["Periods"] = prds
+    # sds, eds, prds = pywapor.pre_se_root.calc_periods(ds_lst.time.values, "3H")
 
-    MERRAmeteo_vars = ['t2m', 'u2m', 'v2m', 'q2m',  'tpw', 'ps', 'slp']
-    GEOSmeteo_vars = ['t2m', 'u2m', 'v2m', 'qv2m', 'tqv', 'ps', 'slp']
+    # dl_args["Startdate"] = sds
+    # dl_args["Enddate"] = eds
+    # dl_args["Periods"] = prds
 
-    # all_files = dict()
-    for var in ["t_air_i", 
-                "u2m_i", "v2m_i", "qv_i", "wv_i", "p_air_i", "p_air_0_i"
-                ]:
+    # MERRAmeteo_vars = ['t2m', 'u2m', 'v2m', 'q2m',  'tpw', 'ps', 'slp']
+    # GEOSmeteo_vars = ['t2m', 'u2m', 'v2m', 'qv2m', 'tqv', 'ps', 'slp']
 
-        files = collect_sources(var, ["GEOS5", "MERRA2"], dl_args, extra_source_locations = None)
+    # # all_files = dict()
+    # for var in ["t_air_i", 
+    #             "u2m_i", "v2m_i", "qv_i", "wv_i", "p_air_i", "p_air_0_i"
+    #             ]:
+
+    #     files = collect_sources(var, ["GEOS5", "MERRA2"], dl_args, extra_source_locations = None)
 
     # print(files)
