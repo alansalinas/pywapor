new biomass calculation (in v2)and memory leak fix

pywapor/et_look_dev/soil_moisture.py
@@ -578,7 +578,10 @@ def wind_speed_blending_height_bare(u_i, z0m_bare=0.001, z_obs=10, z_b=100):
     """
     ws = (c.k * u_i) / np.log(z_obs / z0m_bare) * np.log(z_b / z0m_bare) / c.k
 
-    ws = np.clip(ws, 1, 150)
+    if isinstance(ws, xr.DataArray):
+        ws = ws.clip(1, 150)
+    else:
+        ws = np.clip(ws, 1, 150)
 
     return ws
 
@@ -620,7 +623,10 @@ def wind_speed_blending_height_full_inst(u_i, z0m_full=0.1, z_obs=10, z_b=100):
     """
     ws = (c.k * u_i) / np.log(z_obs / z0m_full) * np.log(z_b / z0m_full) / c.k
 
-    ws = np.clip(ws, 1, 150)
+    if isinstance(ws, xr.DataArray):
+        ws = ws.clip(1, 150)
+    else:
+        ws = np.clip(ws, 1, 150)
 
     return ws
 

pywapor/et_look_v2/biomass.py
@@ -4,8 +4,96 @@
 
 """
 import numpy as np
+import xarray as xr
 
-R_G = 8.3144 # The molar gas constant in J K -1 mol-1
+def fpar(ndvi, fpar_slope=1.257, fpar_offset=-0.161):
+    r"""
+    Computes the fraction of absorbed PAR.
+    PAR is photosynthetically active radiation, which is the solar radiation
+    in the spectral range from 400-700 nm that is used by plants in
+    photosynthesis. This function computes the fpar as a linear function of
+    the ndvi, and is bounded between 0 and 1.
+
+    .. math ::
+        f_{par}=\Delta_{p}*I_{ndvi} + c_{p}
+
+    Parameters
+    ----------
+    ndvi : float
+        normalized difference vegetation index
+        :math:`I_{ndvi}`
+        [-]
+
+    fpar_slope : float
+        slope of the fpar-ndvi curve
+        :math:`\Delta_{p}`
+        [-]
+
+    fpar_offset : float
+        offset of the fpar-ndvi curve
+        :math:`c_{p}`
+        [-]
+
+    Returns
+    -------
+    f_par : float
+        fraction of absorbed photosynthetically active radiation
+        :math:`f_{par}`
+        [-]
+
+    Examples
+    --------
+    >>> import qattara.components.biomass as bio
+    >>> bio.fpar(0.4)
+    0.34179999999999999
+
+    """
+    x = fpar_offset + fpar_slope * ndvi
+
+    if isinstance(x, xr.DataArray):
+        out = x.clip(0.0,1.0)
+    else:
+        out = np.clip(x, 0.0, 1.0)
+    return out
+
+
+def par(ra_24):
+    r"""
+    Computes the photosynthetically active radiation (PAR). PAR is the solar
+    radiation in the spectral range from 400-700 nm that is used by plants
+    for photosynthesis.
+
+    .. math ::
+        PAR=r_{PAR}S^{\downarrow}
+
+    where the following constant is used
+
+    * :math:`r_{PAR}` = ratio par/solar radiation [-] = 0.48
+
+    Parameters
+    ----------
+    ra_24 : float
+       incoming shortwave radiation
+       :math:`S^{\downarrow}`
+       [W/m2]
+
+    Returns
+    -------
+    apar : float
+       photosynthetically active radiation
+       :math:`PAR`
+       [W/m2]
+
+    Examples
+    --------
+    >>> import qattara.components.biomass as bio
+    >>> bio.par(400.0)
+    192.0
+
+    """
+    ABSORBED_RADIATION = 0.48
+
+    return ABSORBED_RADIATION * ra_24
 
 def co2_level_annual(year):
     r"""
@@ -69,6 +157,8 @@ def temperature_dependency(t_air_k_12, dh_ap=52750, d_s=704.98, dh_dp=211000):
         [-]
 
     """
+    R_G = 8.3144 # The molar gas constant in J K -1 mol-1
+
     c1 = 21.77
     x = R_G * t_air_k_12
     y = c1 - dh_ap / x
@@ -94,6 +184,8 @@ def co2_o2_specificity_ratio(t_air_k_12):
         :math:`\tau_CO{2}O{2}`
         [-]
     """
+    R_G = 8.3144 # The molar gas constant in J K -1 mol-1
+
     a_t = 7.87 * 10 ** -5
     e_t = -42869.9
     x = R_G * t_air_k_12
@@ -118,6 +210,8 @@ def inhibition_constant_o2(t_air_k_12):
         :math:`K_0`
         [% O2]
     """
+    R_G = 8.3144 # The molar gas constant in J K -1 mol-1
+
     a_0 = 8240
     e_0 = 13913.5
     x = R_G * t_air_k_12
@@ -146,9 +240,17 @@ def affinity_constant_co2(t_air_k_12):
     a2 = 1.976 * 10 ** 22
     e1 = 59400
     e2 = 109600
-    k_m = np.zeros(t_air_k_12.shape)
-    k_m[t_air_k_12 >= 288.13] = a1 * np.exp(-e1 / (R_G * t_air_k_12[t_air_k_12 >= 288.13]))
-    k_m[t_air_k_12 < 288.13] = a2 * np.exp(-e2 / (R_G * t_air_k_12[t_air_k_12 < 288.13]))
+
+    R_G = 8.3144 # The molar gas constant in J K -1 mol-1
+    
+    if isinstance(t_air_k_12, xr.DataArray):
+        x1 = a1 * np.exp(-e1 / (R_G * t_air_k_12))
+        x2 = a2 * np.exp(-e2 / (R_G * t_air_k_12))
+        k_m = xr.where(t_air_k_12 >= 288.13, x1, x2)
+    else:
+        k_m = np.zeros(t_air_k_12.shape)
+        k_m[t_air_k_12 >= 288.13] = a1 * np.exp(-e1 / (R_G * t_air_k_12[t_air_k_12 >= 288.13]))
+        k_m[t_air_k_12 < 288.13] = a2 * np.exp(-e2 / (R_G * t_air_k_12[t_air_k_12 < 288.13]))
     return k_m
 
 

pywapor/et_look_v2/evapotranspiration.py
@@ -155,7 +155,12 @@ def et_reference_mm(et_ref_24, lh_24):
         [mm d-1]
     """
     x = et_ref_24 * c.day_sec / lh_24
-    et_ref_24_mm = np.clip(x, 0, np.inf)
+
+    if isinstance(x, xr.DataArray):
+        et_ref_24_mm = x.clip(0, np.inf)
+    else:
+        et_ref_24_mm = np.clip(x, 0, np.inf)
+
     return et_ref_24_mm
 
 

pywapor/et_look_v2/leaf.py
@@ -81,7 +81,7 @@ def vegetation_cover(ndvi, nd_min=0.125, nd_max=0.8, vc_pow=0.7):
             res[ndvi >= nd_max] = 1
         else:
             res = 1 - ((nd_max - ndvi) / (nd_max - nd_min)) ** vc_pow
-            res = np.clip(res, 0, 1)
+            res = res.clip(0.0, 1.0)
 
     return res
 

pywapor/et_look_v2/meteo.py
@@ -1296,7 +1296,11 @@ def wind_speed_blending_height(u, z_obs=2, z_b=100):
     z0m = 0.0171
 
     ws = (c.k * u) / np.log(z_obs / z0m) * np.log(z_b / z0m) / c.k
-    ws = np.clip(ws, 1, 150)
+
+    if isinstance(ws, xr.DataArray):
+        ws = ws.clip(1, 150)
+    else:
+        ws = np.clip(ws, 1, 150)    
 
     return ws
 

pywapor/et_look_v2/roughness.py
@@ -123,10 +123,11 @@ def roughness_length(lai, z_oro, z_obst, z_obst_max, land_mask=1):
     else:
         z0m[land_mask == 0] = 0
 
-    z0m = np.where(land_mask == 1, veg_roughness(z_obst, disp, z_obst_max, lai, z_oro), z0m)
     if isinstance(disp, xr.DataArray):
+        z0m = xr.where(land_mask == 1, veg_roughness(z_obst, disp, z_obst_max, lai, z_oro), z0m)
         z0m = xr.where(land_mask == 2, 0.0001, z0m)
     else:
+        z0m = np.where(land_mask == 1, veg_roughness(z_obst, disp, z_obst_max, lai, z_oro), z0m)
         z0m[land_mask == 2] = 0.0001
 
     return z0m

pywapor/et_look_v2/soil_moisture.py
@@ -6,6 +6,7 @@
 from pywapor.et_look_v2 import constants as c
 from pywapor.et_look_v2 import unstable
 import numpy as np
+import xarray as xr
 
 
 def wet_bulb_temperature_inst(t_air_i, t_dew_i, p_air_i):
@@ -546,7 +547,10 @@ def wind_speed_blending_height_bare(u_i, z0m_bare=0.001, z_obs=10, z_b=100):
     """
     ws = (c.k * u_i) / np.log(z_obs / z0m_bare) * np.log(z_b / z0m_bare) / c.k
 
-    ws = np.clip(ws, 1, 150)
+    if isinstance(ws, xr.DataArray):
+        ws = ws.clip(1, 150)
+    else:
+        ws = np.clip(ws, 1, 150)
 
     return ws
 
@@ -589,7 +593,10 @@ def wind_speed_blending_height_full_inst(u_i, z0m_full=0.1, z_obs=10, z_b=100):
     """
     ws = (c.k * u_i) / np.log(z_obs / z0m_full) * np.log(z_b / z0m_full) / c.k
 
-    ws = np.clip(ws, 1, 150)
+    if isinstance(ws, xr.DataArray):
+        ws = ws.clip(1, 150)
+    else:
+        ws = np.clip(ws, 1, 150)
 
     return ws
 
@@ -1136,5 +1143,10 @@ def soil_moisture_from_maximum_temperature(lst_max, lst, lst_min):
 
     """
     ratio = (lst - lst_min) / (lst_max - lst_min)
-    ratio = np.clip(ratio, 0, 1)
+
+    if isinstance(ratio, xr.DataArray):
+        ratio = ratio.clip(0, 1)
+    else:
+        ratio = np.clip(ratio, 0, 1)
+
     return 1 - ratio

pywapor/et_look_v2/stress.py
@@ -1,4 +1,5 @@
 import numpy as np
+import xarray as xr
 
 def stress_radiation(ra_24):
     r"""
@@ -37,7 +38,11 @@ def stress_radiation(ra_24):
     0.90322580645161288
     """
     stress = ra_24/(ra_24 + 60.)*(1 + 60./500.)
-    stress = np.clip(stress, 0, 1)
+    
+    if isinstance(stress, xr.DataArray):
+        stress = stress.clip(0, 1)
+    else:
+        stress = np.clip(stress, 0, 1)
 
     return stress
 
@@ -84,7 +89,11 @@ def stress_moisture(se_root, tenacity=1.5):
     1.0
     """
     stress = tenacity*se_root - (np.sin(2*np.pi*se_root))/(2*np.pi)
-    stress = np.clip(stress, 0, 1)
+
+    if isinstance(stress, xr.DataArray):
+        stress = stress.clip(0,1)
+    else:
+        stress = np.clip(stress, 0, 1)
 
     return stress
 
@@ -147,7 +156,11 @@ def stress_temperature(t_air_24, t_opt=25.0, t_min=0.0, t_max=50.0):
     y = (t_opt - t_min) * (t_max - t_opt)**f
 
     stress = x/y
-    stress = np.clip(stress, 0, 1)
+
+    if isinstance(stress, xr.DataArray):
+        stress = stress.clip(0, 1)
+    else:
+        stress = np.clip(stress, 0, 1)
 
     return stress
 
@@ -192,6 +205,10 @@ def stress_vpd(vpd_24, vpd_slope=-0.3):
 
     """
     stress = vpd_slope * np.log(vpd_24/10. + 0.5) + 1
-    stress = np.clip(stress, 0, 1)
+    
+    if isinstance(stress, xr.DataArray):
+        stress = stress.clip(0, 1)
+    else:
+        stress = np.clip(stress, 0, 1)
 
     return stress