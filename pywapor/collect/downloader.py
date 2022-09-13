3f38121eee1471ea1ab6e045d2a78c37e4c3c930
bert.coerver <bertcoerver@users.noreply.github.com>
2022-09-13 16:12:22 +0200

    extra download exception handling

--- pywapor/collect/downloader.py
+++ pywapor/collect/downloader.py
@@ -1,7 +1,8 @@
-from pywapor.general.logger import log
+from pywapor.general.logger import log, adjust_logger
 import types
 import functools
 import numpy as np
+import sys
 
 def collect_sources(folder, sources, latlim, lonlim, timelim, return_fps = True):
     
@@ -9,43 +10,60 @@ def collect_sources(folder, sources, latlim, lonlim, timelim, return_fps = True)
 
     dss = dict()
 
-    for (source, product_name), req_vars in reversed_sources.items():
-        
-        if isinstance(source, str):
-            dl_module = __import__(f"pywapor.collect.product.{source}", 
-                                fromlist=[source])
-            dler = dl_module.download
-            log.info(f"--> Collecting `{'`, `'.join(req_vars)}` from {source}.{product_name}.")
-            source_name = source
-        elif isinstance(source, types.FunctionType):
-            dler = source
-            log.info(f"--> Collecting `{'`, `'.join(req_vars)}` from `{dler.__name__}`.")
-            source_name = source.__name__
-        elif isinstance(source, functools.partial):
-            dler = source
-            log.info(f"--> Collecting `{'`, `'.join(req_vars)}` from `{dler.func.__name__}`.")
-            source_name = source.func.__name__
+    max_attempts = 3
+    attempts = {k: 0 for k in reversed_sources.keys()}
 
-        log.add()
-        
-        args = {
-            "folder": folder,
-            "latlim": latlim,
-            "lonlim": lonlim,
-            "timelim": timelim,
-            "product_name": product_name,
-            "req_vars": req_vars,
-            "post_processors": reversed_enhancers[(source, product_name)]
-        }
+    while not np.all([v >= max_attempts for v in attempts.values()]):
 
-        x = dler(**args)
-        if "time" in x.coords:
-            stime = np.datetime_as_string(x.time.values[0], unit = "m")
-            etime = np.datetime_as_string(x.time.values[-1], unit = "m")
-            log.add().info(f"> timesize: {x.time.size} [{stime}, ..., {etime}]").sub()
-        dss[(source_name, product_name)] = x
+        reversed_sources = {k: v for k, v in reversed_sources.items() if attempts[k] < max_attempts}
 
-        log.sub()
+        for (source, product_name), req_vars in reversed_sources.items():
+            
+            if isinstance(source, str):
+                dl_module = __import__(f"pywapor.collect.product.{source}", 
+                                    fromlist=[source])
+                dler = dl_module.download
+                log.info(f"--> Collecting `{'`, `'.join(req_vars)}` from {source}.{product_name}.")
+                source_name = source
+            elif isinstance(source, types.FunctionType):
+                dler = source
+                log.info(f"--> Collecting `{'`, `'.join(req_vars)}` from `{dler.__name__}`.")
+                source_name = source.__name__
+            elif isinstance(source, functools.partial):
+                dler = source
+                log.info(f"--> Collecting `{'`, `'.join(req_vars)}` from `{dler.func.__name__}`.")
+                source_name = source.func.__name__
+
+            log.add()
+            
+            args = {
+                "folder": folder,
+                "latlim": latlim,
+                "lonlim": lonlim,
+                "timelim": timelim,
+                "product_name": product_name,
+                "req_vars": req_vars,
+                "post_processors": reversed_enhancers[(source, product_name)]
+            }
+
+            try:
+                x = dler(**args)
+                attempts[(source, product_name)] += max_attempts * 10
+            except Exception as e:
+                if attempts[(source, product_name)] < max_attempts - 1:
+                    log.info(f"--> Attempt {attempts[(source, product_name)] + 1} of {max_attempts} failed, trying again after other sources have been collected. ({type(e).__name__}: {e}).")
+                else:
+                    log.info(f"--> Attempt {attempts[(source, product_name)] + 1} of {max_attempts} failed, giving up now. ({type(e).__name__}: {e}).")
+                attempts[(source, product_name)] += 1
+            else:
+                if "time" in x.coords:
+                    stime = np.datetime_as_string(x.time.values[0], unit = "m")
+                    etime = np.datetime_as_string(x.time.values[-1], unit = "m")
+                    log.add().info(f"> timesize: {x.time.size} [{stime}, ..., {etime}]").sub()
+                dss[(source_name, product_name)] = x
+            finally:
+                log.sub()
+                continue
 
     if return_fps:
         for key, ds in dss.items():
@@ -72,47 +90,23 @@ def reverse_sources(sources):
 
     return reversed_sources, reversed_enhancers
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
 
-#     import datetime
+    import datetime
 
-#     sources = {
+    sources = {
 
-#         "ndvi":         [("MODIS", "MOD13Q1.061"), ("MODIS", "MYD13Q1.061")],
-#         "r0":           [("MODIS", "MCD43A3.061")],
-#         # "lst":          [("MODIS", "MOD11A1.061"), ("MODIS", "MYD11A1.061")],
+        "r0":           {"products": [{"source": "MODIS", "product_name": "MCD43A3.061", "enhancers": "default"}]},
+        "z":            {"products": [{"source": "SRTM", "product_name": "30M", "enhancers": "default"}]},
+        "p":            {"products": [{"source": "CHIRPS", "product_name": "P05", "enhancers": "default"}]},
+        "lw_offset":    {"products": [{"source": "STATICS", "product_name": "WaPOR2", "enhancers": "default"}]},
+    }
 
-#         "z":            [("SRTM", "30M")],
-#         "p":            [("CHIRPS", "P05")],
-#         "ra":           [("MERRA2", "M2T1NXRAD.5.12.4")],
-#         "t_air":        [("MERRA2", "M2I1NXASM.5.12.4")],
-#         "t_air_max":    [("MERRA2", "M2I1NXASM.5.12.4")],
-#         # "u2m":          [("MERRA2", "M2I1NXASM.5.12.4")],
-#         # "v2m":          [("MERRA2", "M2I1NXASM.5.12.4")],
-#         # "qv":           [("MERRA2", "M2I1NXASM.5.12.4")],
-#         # "wv":           [("MERRA2", "M2I1NXASM.5.12.4")],
-#         # "p_air":        [("MERRA2", "M2I1NXASM.5.12.4")],
-#         # "p_air_0":      [("MERRA2", "M2I1NXASM.5.12.4")],
+    folder = r"/Users/hmcoerver/Local/supersafe_dler"
+    latlim = [28.9, 29.7]
+    lonlim = [30.2, 31.2]
+    timelim = [datetime.date(2020, 7, 1), datetime.date(2020, 7, 15)]
 
-#         # "lw_offset":    [("STATICS", "lw_offset")],
-#         # "lw_slope":     [("STATICS", "lw_slope")],
-#         # "r0_bare":      [("STATICS", "r0_bare")],
-#         # "r0_full":      [("STATICS", "r0_full")],
-#         # "rn_offset":    [("STATICS", "rn_offset")],
-#         # "rn_slope":     [("STATICS", "rn_slope")],
-#         # "t_amp_year":   [("STATICS", "t_amp_year")],
-#         # "t_opt":        [("STATICS", "t_opt")],
-#         # "vpd_slope":    [("STATICS", "vpd_slope")],
-#         # "z_oro":        [("STATICS", "z_oro")],
+    adjust_logger(True, folder, "INFO")
 
-#         # "level_name":   "sideloading",
-#     }
-
-    # folder = r"/Users/hmcoerver/Downloads/pywapor_test"
-    # latlim = [26.9, 33.7]
-    # lonlim = [25.2, 37.2]
-    # latlim = [28.9, 29.7]
-    # lonlim = [30.2, 31.2]
-    # timelim = [datetime.date(2020, 7, 1), datetime.date(2020, 8, 1)]
-
-    # dss0 = collect_sources(folder, sources, latlim, lonlim, timelim)
+    dss0 = collect_sources(folder, sources, latlim, lonlim, timelim)
