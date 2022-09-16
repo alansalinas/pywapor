9a1ad0f7b6dd4fa2180421925fe23f1296bbdef3
bert.coerver <bertcoerver@users.noreply.github.com>
2022-09-16 17:15:58 +0200

    thermal sharpener

--- pywapor/enhancers/dms/thermal_sharpener.py
+++ pywapor/enhancers/dms/thermal_sharpener.py
@@ -2,11 +2,11 @@ import os
 import xarray as xr
 from osgeo import gdal
 import datetime
-from pywapor.general.processing_functions import save_ds, remove_ds
+from pywapor.general.processing_functions import save_ds, remove_ds, open_ds
 from pywapor.enhancers.dms.pyDMS import DecisionTreeSharpener
-import glob
 import xml.etree.ElementTree as ET
 import pandas as pd
+from pywapor.general.logger import log, adjust_logger
 
 def gdal_stop_warnings_and_raise_errors():
     """https://gis.stackexchange.com/questions/43404/how-to-detect-a-gdal-ogr-warning/68042
@@ -71,7 +71,7 @@ def lowres_inputs(workdir, temporal_lr_input_data):
     if not os.path.exists(workdir):
         os.makedirs(workdir)
         
-    ds = xr.open_dataset(temporal_lr_input_data[0].split(":")[1])
+    ds = xr.open_dataset(temporal_lr_input_data.split(":")[1])
     times = [x.strftime("%Y%m%d_%H%M%S") for x in pd.to_datetime(ds.time.values)]
     ds = ds.close()
 
@@ -81,7 +81,7 @@ def lowres_inputs(workdir, temporal_lr_input_data):
         output_vrt = os.path.join(workdir, f"lowres_input_{dt}.vrt")
         if os.path.isfile(output_vrt):
             os.remove(output_vrt)
-        ds = gdal.BuildVRT(output_vrt, temporal_lr_input_data, options = buildvrt_options)
+        ds = gdal.BuildVRT(output_vrt, [temporal_lr_input_data], options = buildvrt_options)
         ds.FlushCache()
         ds = None
         fps.append(output_vrt)
@@ -91,6 +91,60 @@ def lowres_inputs(workdir, temporal_lr_input_data):
 def preprocess(ds):
     dt = datetime.datetime.strptime(os.path.split(ds.encoding["source"])[-1], "highres_output_%Y%m%d_%H%M%S.nc")
     ds["Band1"] = ds["Band1"].expand_dims({"time": 1}).assign_coords({"time": [dt]})
+    return ds
+
+def sharpen(ds, var, dss, folder, vars_for_sharpening = ['nmdi', 'bsi', 'mndwi', 
+                'vari_red_edge', 'psri', 'nir', 'green', 'z', 'aspect', 'slope']):
+
+    missing_vars = [x for x in vars_for_sharpening if x not in dss.keys()]
+    if len(missing_vars) > 0:
+        log.info(f"--> Sharpening `{var}` without `{'` and `'.join(missing_vars)}`.")
+
+    workdir = os.path.join(folder, "DMS")
+
+    temporal_lr_input_data = f"NETCDF:{ds.encoding['source']}:{var}"
+    temporal_hr_input_data = [f"NETCDF:{ds.encoding['source']}:{x}" for x, ds in dss.items() if "time" in ds.dims and x in vars_for_sharpening]
+    static_hr_input_data = [f"NETCDF:{ds.encoding['source']}:{x}" for x, ds in dss.items() if "time" not in ds.dims and x in vars_for_sharpening]
+
+    highResFiles = sorted(highres_inputs(workdir, temporal_hr_input_data, static_hr_input_data))
+    lowResFiles = sorted(lowres_inputs(workdir, temporal_lr_input_data))
+
+    out_fns = list()
+    log.info(f"--> Sharpening {len(highResFiles)} `{var}` images.").add()
+    for i, (highResFilename, lowResFilename) in enumerate(zip(highResFiles, lowResFiles)):
+        fp, fn = os.path.split(highResFilename)
+
+        log.info(f"--> ({i+1}/{len(highResFiles)}) Sharpening `{os.path.split(lowResFilename)[-1]}` with `{fn}`.").add()
+
+        _, correctedImage = thermal_sharpen(highResFilename, lowResFilename)
+
+        log.sub()
+
+        fp_out = os.path.join(fp, fn.replace("input", "output").replace(".vrt", ".nc"))
+        ds = gdal.Translate(fp_out, correctedImage)
+        ds.FlushCache()
+        ds = None
+
+        out_fns.append(fp_out)
+
+    log.sub()
+
+    ds = xr.open_mfdataset(out_fns, preprocess = preprocess, decode_coords = "all")
+    ds = ds.rename_dims({"lat": "y", "lon": "x"})
+    ds = ds.transpose("time", "y", "x")
+    ds = ds.rename_vars({"crs": "spatial_ref", "Band1": var, "lat": "y", "lon": "x"})
+    ds = ds.rio.write_grid_mapping("spatial_ref")
+    ds = ds.sortby("y", ascending = False)
+    ds = ds.sortby("x")
+    ds.attrs = {}
+
+    fp = os.path.join(workdir, f"{var}_i.nc")
+
+    ds = save_ds(ds, fp, encoding = "initiate", label = f"Merging sharpened `{var}` files.")
+
+    for fp in out_fns:
+        remove_ds(fp)
+
     return ds
 
 def thermal_sharpen(highres_fn, lowres_fn):
@@ -122,50 +176,26 @@ def thermal_sharpen(highres_fn, lowres_fn):
 
 if __name__ == "__main__":
 
-    bt_nc = r"/Users/hmcoerver/Local/therm_sharpener/VIIRSL1/VNP02IMG.nc"
     folder = r"/Users/hmcoerver/Local/therm_sharpener"
-    workdir = r"/Users/hmcoerver/Local/therm_sharpener/DMS"
-    s2_ncs = glob.glob(os.path.join(folder, "*.nc"))
-    z_nc = r"/Users/hmcoerver/Local/therm_sharpener/COPERNICUS/GLO90.nc"
 
-    temporal_lr_input_data = f"NETCDF:{bt_nc}:bt"
-    temporal_hr_input_data = [f"NETCDF:{fp}:{var}" for fp, var in [(fp, "_".join(os.path.split(fp)[-1].split("_")[0:-1])) for fp in s2_ncs]]
-    static_hr_input_data = [f"NETCDF:{z_nc}:z", f"NETCDF:{z_nc}:slope", f"NETCDF:{z_nc}:aspect"]
+    adjust_logger(True, folder, "INFO")
 
-    highResFiles = highres_inputs(workdir, temporal_hr_input_data, static_hr_input_data)
-    lowResFiles = lowres_inputs(workdir, temporal_lr_input_data)
-
-    fldr = r"/Users/hmcoerver/Local/therm_sharpener/DMS"
+    vars_for_sharpening = ['nmdi', 'bsi', 'mndwi', 'vari_red_edge', 'psri', 'nir', 'green', 'z', 'aspect', 'slope']
 
     var = "bt"
+    ds = open_ds(r"/Users/hmcoerver/Local/therm_sharpener/VIIRSL1/VNP02IMG.nc")
 
-    highResFilenames = sorted(glob.glob(os.path.join(workdir, "highres*.vrt")))
-    lowResFilenames = sorted(glob.glob(os.path.join(workdir, "lowres*.vrt")))
+    dss = {
+        'nmdi':             open_ds('/Users/hmcoerver/Local/therm_sharpener/nmdi_i.nc'),
+        'bsi':              open_ds('/Users/hmcoerver/Local/therm_sharpener/bsi_i.nc'),
+        'mndwi':            open_ds('/Users/hmcoerver/Local/therm_sharpener/mndwi_i.nc'),
+        'vari_red_edge':    open_ds('/Users/hmcoerver/Local/therm_sharpener/vari_red_edge_i.nc'),
+        'psri':             open_ds('/Users/hmcoerver/Local/therm_sharpener/psri_i.nc'),
+        'nir':              open_ds('/Users/hmcoerver/Local/therm_sharpener/nir_i.nc'),
+        'green':            open_ds('/Users/hmcoerver/Local/therm_sharpener/green_i.nc'),
+        'z':                open_ds('/Users/hmcoerver/Local/therm_sharpener/COPERNICUS/GLO90.nc'),
+        'aspect':           open_ds('/Users/hmcoerver/Local/therm_sharpener/COPERNICUS/GLO90.nc'),
+        'slope':            open_ds('/Users/hmcoerver/Local/therm_sharpener/COPERNICUS/GLO90.nc'),
+        }
 
-    out_fns = list()
-    for highResFilename, lowResFilename in zip(highResFilenames, lowResFilenames):
-        residualImage, correctedImage = thermal_sharpen(highResFilename, lowResFilename)
-        
-        fp, fn = os.path.split(highResFilename)
-        fp_out = os.path.join(fp, fn.replace("input", "output").replace(".vrt", ".nc"))
-        ds = gdal.Translate(fp_out, correctedImage)
-        ds.FlushCache()
-        ds = None
-
-        out_fns.append(fp_out)
-
-    ds = xr.open_mfdataset(out_fns, preprocess = preprocess, decode_coords = "all")
-    ds = ds.rename_dims({"lat": "y", "lon": "x"})
-    ds = ds.transpose("time", "y", "x")
-    ds = ds.rename_vars({"crs": "spatial_ref", "Band1": var, "lat": "y", "lon": "x"})
-    ds = ds.rio.write_grid_mapping("spatial_ref")
-    ds = ds.sortby("y", ascending = False)
-    ds = ds.sortby("x")
-    ds.attrs = {}
-
-    fp = os.path.join(workdir, f"{var}_i.nc")
-
-    ds = save_ds(ds, fp, encoding = "initiate", label = f"Merging sharpened `{var}` files.")
-
-    for fp in out_fns:
-        remove_ds(fp)
+    out = sharpen(ds, var, dss, folder)
