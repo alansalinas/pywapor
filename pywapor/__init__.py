a64849e34b9f360552c54f11c5df585a1382bbe1
bert.coerver <bertcoerver@users.noreply.github.com>
2023-01-04 11:58:07 +0100

    version number

--- pywapor/__init__.py
+++ pywapor/__init__.py
@@ -7,4 +7,4 @@ from pywapor import general, et_look_dev, et_look_v2_v3, pre_et_look, et_look, c
 
 __all__ = ['general', 'et_look_dev', 'et_look_v2_v3', 'pre_et_look', 'et_look', 'collect', 'post_et_look', 'pre_se_root', 'se_root', 'enhancers']
 
-__version__ = '3.1.5'
+__version__ = '3.2.1'
