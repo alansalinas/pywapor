ab726b06ce7d56ca039a87f0f061a9165fcea1bc
bert.coerver <bertcoerver@users.noreply.github.com>
2023-09-22 16:07:19 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pywapor',
-    version = '3.4.1',
+    version = '3.4.2',
     url = 'https://www.fao.org/aquastat/py-wapor/',
     author = "FAO",
     author_email = "bert.coerver@fao.org",
