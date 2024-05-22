5f029c66111112517c2f0b88a0cc7ed33aeeec26
bert.coerver <bertcoerver@users.noreply.github.com>
2024-05-22 15:58:34 +0200

    version bump

--- pywapor/__init__.py
+++ pywapor/__init__.py
@@ -8,4 +8,4 @@ from pywapor.main import Project
 
 __all__ = ['general', 'et_look_v2_v3', 'pre_et_look', 'et_look', 'collect', 'post_et_look', 'pre_se_root', 'se_root', 'enhancers', "Project"]
 
-__version__ = '3.5.1'
+__version__ = '3.5.2'
